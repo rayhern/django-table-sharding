d1c1942ff623416c884629b981081448f772f76d
rayhern <bizong@gmail.com>
2020-07-04 02:26:28 -0700

    Added create() method and bulk_create() method in ShardManager. Also, overwrote save() on model instance to be compatible with shards.

--- django_table_sharding/managers.py
+++ django_table_sharding/managers.py
@@ -1,6 +1,14 @@
-from django.db import models, connections
+from django.db import connections
 from django.db.models.expressions import Col
 from django.db import models
+import datetime
+import traceback
+
+
+def chunks(source_list, batch_size):
+    """Yield successive n-sized chunks from source_list."""
+    for i in range(0, len(source_list), batch_size):
+        yield source_list[i:i+batch_size]
 
 
 class ShardException(Exception):
@@ -40,6 +48,83 @@ class ShardManager(models.Manager):
             f.cached_col = Col(meta.db_table, f)
         return super(ShardManager, self).using(self.db)
 
+    def create(self, table_suffix, db='default', **kwargs):
+        meta = getattr(self.model, '_meta')
+        db_table = '%s_%s_%s' % (
+            str(meta.app_label),
+            str(self.model.__name__.lower()), table_suffix)
+
+        for k, v in kwargs.items():
+            if isinstance(v, datetime.datetime):
+                kwargs[k] = v.strftime('%Y-%m-%d %H:%M:%S')
+            elif isinstance(v, datetime.date):
+                kwargs[k] = v.strftime('%Y-%m-%d')
+            elif isinstance(v, datetime.time):
+                kwargs[k] = v.strftime('%H:%M:%S')
+            elif isinstance(v, bool):
+                if v is True:
+                    kwargs[k] = 1
+                else:
+                    kwargs[k] = 0
+
+        placeholders = ', '.join(['%s'] * len(kwargs.values()))
+        columns = ', '.join(kwargs.keys())
+        tuple_list = tuple(kwargs.values())
+
+        try:
+            with connections[db].cursor() as cursor:
+                try:
+                    cursor.execute("INSERT IGNORE INTO %s ( %s ) VALUES ( %s )" % (
+                        db_table, columns, placeholders), tuple_list)
+                except:
+                    print(traceback.format_exc())
+        except:
+            raise ShardException(traceback.format_exc())
+
+    def bulk_create(self, table_suffix, list_of_dicts, batch_size=5, ignore_conflicts=False, db='default'):
+        if len(list_of_dicts) == 0:
+            raise ShardException('List of dict field values not defined.')
+
+        meta = getattr(self.model, '_meta')
+        db_table = '%s_%s_%s' % (
+            str(meta.app_label),
+            str(self.model.__name__.lower()), table_suffix)
+
+        for dict_fields in list_of_dicts:
+            for k, v in dict_fields.items():
+                if isinstance(v, datetime.datetime):
+                    dict_fields[k] = v.strftime('%Y-%m-%d %H:%M:%S')
+                elif isinstance(v, datetime.date):
+                    dict_fields[k] = v.strftime('%Y-%m-%d')
+                elif isinstance(v, datetime.time):
+                    dict_fields[k] = v.strftime('%H:%M:%S')
+                elif isinstance(v, bool):
+                    if v is True:
+                        dict_fields[k] = 1
+                    else:
+                        dict_fields[k] = 0
+
+        placeholders = ', '.join(['%s'] * len(list_of_dicts[0]))
+        columns = ', '.join(list_of_dicts[0].keys())
+
+        n_chunks = chunks(list_of_dicts, batch_size)
+        for chunk in n_chunks:
+            tuple_list = [tuple(d.values()) for d in chunk]
+            if ignore_conflicts is False:
+                with connections[db].cursor() as cursor:
+                    try:
+                        cursor.executemany("INSERT INTO %s ( %s ) VALUES ( %s )" % (
+                            db_table, columns, placeholders), tuple_list)
+                    except:
+                        raise ShardException(traceback.format_exc())
+            else:
+                with connections[db].cursor() as cursor:
+                    try:
+                        cursor.executemany("INSERT IGNORE INTO %s ( %s ) VALUES ( %s )" % (
+                            db_table, columns, placeholders), tuple_list)
+                    except:
+                        raise ShardException(traceback.format_exc())
+
     def shard_exists(self, table_suffix, db='default'):
         """
         Check if sharded table exists.
@@ -72,15 +157,15 @@ class ShardManager(models.Manager):
 
 class ShardedModel(models.Model):
     def save(self, *args, **kwargs):
-        if 'shard' in kwargs:
-            shard = kwargs.pop('shard')
-            db_table = '%s_%s_%s' % (self._meta.app_label, self._meta.verbose_name, shard)
+        if len(args) > 0:
+            shard = args[0]
+            db_table = '%s_%s_%s' % (self._meta.app_label, str(self._meta.model.__name__).lower(), shard)
             self._meta.db_table = db_table
             for f in self._meta.concrete_fields:
                 f.cached_col = Col(self._meta.db_table, f)
-            super(ShardedModel, self).save(*args, **kwargs)
+            return super(ShardedModel, self).save(**kwargs)
         else:
-            raise ShardException('No shard specified in save.')
+            raise ShardException('No shard/table suffix specified in save.')
 
     class Meta:
         abstract = True
